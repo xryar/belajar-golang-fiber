4658b283073271c0a6260d93e731969e3a5feb48
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-06-18 13:10:06 +0700

    fiber route parameter

--- fiber_test.go
+++ fiber_test.go
@@ -70,3 +70,20 @@ func TestHttpRequest(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello Arya Andaru", string(bytes))
 }
+
+func TestRouteParameter(t *testing.T) {
+	app.Get("/users/:userId/orders/:orderId", func(ctx *fiber.Ctx) error {
+		userId := ctx.Params("userId")
+		orderId := ctx.Params("orderId")
+		return ctx.SendString("Get Order " + orderId + " From User " + userId)
+	})
+
+	request := httptest.NewRequest("GET", "/users/acumalaka/orders/10", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Get Order 10 From User acumalaka", string(bytes))
+}
